943b5b12eb110cbdaf9d4f9a694538fecabf3cbb
unknown <sanmithasadhishkumar@gmail.com>
2024-03-20 23:42:43 +0530

    linked with firebase

--- app/lib/main.dart
+++ app/lib/main.dart
@@ -2,13 +2,19 @@ import 'package:flutter/material.dart';
 import 'package:flutter/painting.dart';
 import 'screens/HomeLayout.dart';
 import 'colorThemes.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
 
-void main() {
+void main() async {
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
+
 
   // This widget is the root of your application.
   @override
